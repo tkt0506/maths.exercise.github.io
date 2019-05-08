b83b8ecb85a1fdf59482c2e5b93bd44205ec9330
tkt0506 <46648701+tkt0506@users.noreply.github.com>
2019-05-08 15:10:25 +0800

    Update index.java

--- index.java
+++ index.java
@@ -1,4 +1,4 @@
-public class MyClass {
+xpublic class MyClass {
   public static void main(String[] args) {
     System.out.println("hello everyone,this is my  second blog.But also very bad");
   }
@@ -6,6 +6,18 @@ public class MyClass {
 public class MyClass {
   public static void main(String[] args) {
     System.out.println(Math.random());  
+  }
+}
+public class MyClass {
+  public static void main(String[] args) {
+    int time = 22;
+    if (time < 10) {
+      System.out.println("Good morning.");
+    } else if (time < 20) {
+      System.out.println("Good day.");
+    }  else {
+      System.out.println("Good evening.");
+    }
   }
 }
 
